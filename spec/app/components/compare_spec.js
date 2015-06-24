d1cb224d21fc59e86421e644804f5a3632b24da2
Charles Hansen <chansen@pivotal.io>
2015-06-24 09:44:04 -0700

    add a compare spec refactor from page spec
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

---
+++ spec/app/components/compare_spec.js
@@ -0,0 +1,80 @@
+require('../spec_helper');
+
+describe('Compare', function() {
+  var DrugLabelApi, searchDeferred, context;
+  beforeEach(function() {
+    DrugLabelApi = require('../../../app/api/drug_label_api');
+    searchDeferred = new Deferred();
+    spyOn(DrugLabelApi, 'search').and.returnValue(searchDeferred.promise());
+    var Compare = require('../../../app/components/compare');
+    var $application = new Cursor({page: 'compare', existingDrugs: [], search: null}, jasmine.createSpy('drugLabels'));
+    context = withContext({config: {}}, {$application}, function() {
+      var {$application} = this.props;
+      return (<Compare {...{$application}}/>);
+    }, root);
+  });
+
+  afterEach(function() {
+    React.unmountComponentAtNode(root);
+  });
+
+  it('renders a search drug label', function() {
+    expect('.search-drug-label').toExist();
+  });
+
+  it('renders the drug list', function() {
+    expect('.existing-drugs-list').toExist();
+  });
+
+  it('does not render the submit button', function() {
+    expect(':submit').not.toExist();
+  });
+
+  it('does not render a new drug', function() {
+    expect('.new-drug').not.toExist();
+  });
+
+  describe('when there is search', function() {
+    beforeEach(function() {
+      var $application = new Cursor({page: 'compare', existingDrugs: ['ibuprofen'], search: 'ibuprofen'}, jasmine.createSpy('callback'));
+      context.setProps({$application});
+    });
+
+    it('enables the submit button', function() {
+      expect(':submit:disabled').not.toExist();
+    });
+
+    describe('when submitting the search', function() {
+      beforeEach(function() {
+        $(':submit').simulate('submit');
+      });
+
+      it('makes an DrugLabel search api request', function() {
+        expect(DrugLabelApi.search).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('when there are drug labels', function() {
+    beforeEach(function() {
+      var $application = new Cursor({page: 'compare', existingDrugs: ['ibuprofen'], search: 'ibuprofen'}, jasmine.createSpy('callback'));
+      context.setProps({$application});
+    });
+
+    it('renders the results', function() {
+      expect('.existing-drugs-list').toExist();
+      expect('.existing-drugs-list').toContainText('ibuprofen');
+    });
+  });
+
+  describe('when there is a new drug', function() {
+    beforeEach(function() {
+      var $application = new Cursor({page: 'compare', existingDrugs: ['ibuprofen'], newDrug: 'claritin', search: 'ibuprofen'}, jasmine.createSpy('callback'));
+      context.setProps({$application});
+    });
+
+    it('renders a new drug', function() {
+      expect('.new-drug').toExist();
+    });
+  });
+});
--- spec/app/components/page_spec.js
+++ spec/app/components/page_spec.js
@@ -1,14 +1,16 @@
 require('../spec_helper');
 
 describe('Page', function() {
-  var DrugLabelApi, searchDeferred, context;
+  var Compare, DrugLabelApi, searchDeferred;
   beforeEach(function() {
     DrugLabelApi = require('../../../app/api/drug_label_api');
     searchDeferred = new Deferred();
     spyOn(DrugLabelApi, 'search').and.returnValue(searchDeferred.promise());
+    Compare = require('../../../app/components/compare');
+    spyOn(Compare.prototype, 'render').and.callThrough();
     var Page = require('../../../app/components/page');
     var $application = new Cursor({page: 'compare', existingDrugs: [], search: null}, jasmine.createSpy('drugLabels'));
-    context = withContext({config: {}}, {$application}, function() {
+    withContext({config: {}}, {$application}, function() {
       var {$application} = this.props;
       return (<Page {...{$application}}/>);
     }, root);
@@ -18,63 +20,7 @@ describe('Page', function() {
     React.unmountComponentAtNode(root);
   });
 
-  it('renders a search drug label', function() {
-    expect('.search-drug-label').toExist();
-  });
-
-  it('renders the drug list', function() {
-    expect('.existing-drugs-list').toExist();
-  });
-
-  it('does not render the submit button', function() {
-    expect(':submit').not.toExist();
-  });
-
-  it('does not render a new drug', function() {
-    expect('.new-drug').not.toExist();
-  });
-
-  describe('when there is search', function() {
-    beforeEach(function() {
-      var $application = new Cursor({page: 'compare', existingDrugs: ['ibuprofen'], search: 'ibuprofen'}, jasmine.createSpy('callback'));
-      context.setProps({$application});
-    });
-
-    it('enables the submit button', function() {
-      expect(':submit:disabled').not.toExist();
-    });
-
-    describe('when submitting the search', function() {
-      beforeEach(function() {
-        $(':submit').simulate('submit');
-      });
-
-      it('makes an DrugLabel search api request', function() {
-        expect(DrugLabelApi.search).toHaveBeenCalled();
-      });
-    });
-  });
-
-  describe('when there are drug labels', function() {
-    beforeEach(function() {
-      var $application = new Cursor({page: 'compare', existingDrugs: ['ibuprofen'], search: 'ibuprofen'}, jasmine.createSpy('callback'));
-      context.setProps({$application});
-    });
-
-    it('renders the results', function() {
-      expect('.existing-drugs-list').toExist();
-      expect('.existing-drugs-list').toContainText('ibuprofen');
-    });
-  });
-
-  describe('when there is a new drug', function() {
-    beforeEach(function() {
-      var $application = new Cursor({page: 'compare', existingDrugs: ['ibuprofen'], newDrug: 'claritin', search: 'ibuprofen'}, jasmine.createSpy('callback'));
-      context.setProps({$application});
-    });
-
-    it('renders a new drug', function() {
-      expect('.new-drug').toExist();
-    });
+  it('renders the compare component', function() {
+    expect(Compare.prototype.render).toHaveBeenCalled();
   });
 });
