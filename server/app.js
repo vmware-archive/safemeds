9a6e2a04a5ef39715a9b8187703139a09c8c8562
Charles Hansen <chansen@pivotal.io>
2015-06-29 14:33:08 -0700

    adding compression for the express server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var Application = require('../app/components/application');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 var cookieParser = require('cookie-parser');
 var express = require('express');
 var gzipStatic = require('connect-gzip-static');
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(gzipStatic(`${__dirname}/../public`, {maxAge: process.env.NODE_ENV === 'production' && 604800000}));
+app.use(compression());
 
 app.get('/', ...component.show(Application, 'application'));
 
