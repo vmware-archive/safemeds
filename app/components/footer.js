78bebb89a6010f833ab3bd2cd1b7b285b0d7a4c4
Vinson Chuong <vchuong@pivotal.io>
2015-07-01 10:06:59 -0700

    [#98153460 #98064118 #98029540] Visual updates while pairing with Pam
    Signed-off-by: Vinson Chuong <vchuong@pivotal.io>

--- app/components/footer.js
+++ app/components/footer.js
@@ -5,11 +5,11 @@ var Footer = React.createClass({
   render() {
     return (
       <footer>
-        <div className="svg"><Svg src="alert-pill"/></div>
+        <div className="svg"><Svg src="pill-bottle-disclaimer"/></div>
         <span className="arrow"/>
         <span className="disclaimer">Disclaimer</span>
         <p>Always consult your doctor before starting a new medication.</p>
-        <span className="fine-print">This site is for informative information only.</span>
+        <span className="fine-print">This site is for informational purposes only.</span>
       </footer>
     );
   }
